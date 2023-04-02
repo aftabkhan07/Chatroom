f5e1f079d2e45f6cb12d5917671ad5ffafe753a8
aftabkhan07 <khanaftaba1@gmail.com>
2023-04-02 18:02:57 +0530

    few more features added

--- base/urls.py
+++ base/urls.py
@@ -3,12 +3,15 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register'),
 
     path('', views.home, name='home'),
     path('room/<str:pk>/',views.room, name='room'),
     path('create-room',views.createroom, name='create-room'),
     path('update-room/<str:pk>/',views.updateroom, name='update-room'),
     path('delete-room/<str:pk>/',views.deleteroom, name='delete-room'),
+    path('delete-message/<str:pk>/',views.deleteMessage, name='delete-message'),
 ]
 
 
