a03cfa944263c142f6b113027e17147530628ac4
aftabkhan07 <khanaftaba1@gmail.com>
2023-04-10 22:55:16 +0530

    Few more changes and adding static file

--- base/urls.py
+++ base/urls.py
@@ -8,12 +8,14 @@ urlpatterns = [
 
     path('', views.home, name='home'),
     path('room/<str:pk>/',views.room, name='room'),
+    path('profile/<str:pk>/',views.userProfile, name='user-profile'),
+
     path('create-room',views.createroom, name='create-room'),
     path('update-room/<str:pk>/',views.updateroom, name='update-room'),
     path('delete-room/<str:pk>/',views.deleteroom, name='delete-room'),
     path('delete-message/<str:pk>/',views.deleteMessage, name='delete-message'),
 ]
-
+  
 
 
 
